0dba6bc6aa68cd12fca126537ad80190e5182bd5
Evan32nd <eqlh1998@gmail.com>
2022-03-21 20:02:16 -0600

    Added simple js app files

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+/*let myName = 'Bob ';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);*/
+
+alert('Hello World');
+
+let favoriteFood = 'Meat!'
+document.write(favoriteFood);
